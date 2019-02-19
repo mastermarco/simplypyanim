a7dbe6db94040d4e40fe5161a1c8e334fe4b4fe1
mastermarco <marcogirasoli@outlook.it>
2019-02-19 23:25:24 +0100

    adafruit example code

--- Config.py
+++ Config.py
@@ -1,7 +1,12 @@
 import pygame
 from pygame import *
+import os
+os.putenv('SDL_FBDEV', '/dev/fb1')
+#os.putenv('FBDEV', '/dev/fb1')
 
 WIDTH = 160
 HEIGHT = 128
 
-DISPLAYSURFACE = pygame.display.set_mode((WIDTH, HEIGHT), 0, 8)
+#DISPLAYSURFACE = pygame.display.set_mode((WIDTH, HEIGHT), pygame.FULLSCREEN, 0)
+#DISPLAYSURFACE = pygame.display.set_mode((WIDTH, HEIGHT), 0, 0)
+DISPLAYSURFACE = pygame.display.set_mode((WIDTH, HEIGHT))
